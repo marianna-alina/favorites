91074c2f8a541fda7872e9eaf26c79a88d80012f
Alina Havriushenko <alinagvr@gmail.com>
2024-02-16 18:11:53 +0100

    add category card on dashboard

--- src/assets/pages/CategoryPage.jsx
+++ src/assets/pages/CategoryPage.jsx
@@ -4,51 +4,51 @@ import axios from "axios";
 import ItemCard from "../components/ItemCard";
 
 export default function CategoryPage() {
-  const [category, setCategory] = useState(null);
-  const [items, setItems] = useState(null);
-  const API_URL = "https://json-server-backend-app.adaptable.app";
+    const [category, setCategory] = useState(null);
+    const [items, setItems] = useState(null);
+    const API_URL = "https://json-server-backend-app.adaptable.app";
 
-  const { categoryID } = useParams();
+    const { categoryID } = useParams();
 
-  useEffect(() => {
-    axios
-      .get(`${API_URL}/categories/${categoryID}`)
-      .then((response) => {
-        setCategory(response.data);
-      })
-      .catch((e) => console.log(e));
-  }, [categoryID]);
+    useEffect(() => {
+        axios
+            .get(`${API_URL}/categories/${categoryID}`)
+            .then((response) => {
+                setCategory(response.data);
+            })
+            .catch((e) => console.log(e));
+    }, [categoryID]);
 
-  useEffect(() => {
-    axios
-      .get(`${API_URL}/items`)
-      .then((response) => {
-        setItems(response.data);
-      })
-      .catch((e) => console.log(e));
-  }, [categoryID]);
+    useEffect(() => {
+        axios
+            .get(`${API_URL}/items`)
+            .then((response) => {
+                setItems(response.data);
+            })
+            .catch((e) => console.log(e));
+    }, [categoryID]);
 
-  const filterItems = (array) => {
-    if (array) {
-      return array.filter((item) => {
-        return item.category_id == categoryID;
-      });
-    }
-  };
+    const filterItems = (array) => {
+        if (array) {
+            return array.filter((item) => {
+                return item.category_id == categoryID;
+            });
+        }
+    };
 
-  return (
-    <>
-      {category === null ? <p>Loading</p> : <h1>{category.name}</h1>}
+    return (
+        <>
+            {category === null ? <p>Loading</p> : <h1>{category.name}</h1>}
 
-      {items === null ? (
-        <p>Loading...</p>
-      ) : (
-        filterItems(items).map((filteredItem) => (
-          <Link key={filteredItem.id} to={`items/${filteredItem.id}`}>
-            <ItemCard item={filteredItem} />
-          </Link>
-        ))
-      )}
-    </>
-  );
+            {items === null ? (
+                <p>Loading...</p>
+            ) : (
+                filterItems(items).map((filteredItem) => (
+                    <Link key={filteredItem.id} to={`items/${filteredItem.id}`}>
+                        <ItemCard item={filteredItem} />
+                    </Link>
+                ))
+            )}
+        </>
+    );
 }
--- src/assets/pages/Dashboard.jsx
+++ src/assets/pages/Dashboard.jsx
@@ -1,11 +1,38 @@
-export default function Dashboard() {
-  return (
-    <>
-      <h1>This is Dashboard</h1>
-      <p>add card for each category</p>
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useParams } from "react-router-dom"
+import axios from "axios"
 
-      {/* < CategoryCard /> */}
-      {/* redirects to category page */}
-    </>
-  );
+import CategoryCard from "../components/CategoryCard"
+
+
+export default function Dashboard() {
+
+    const [category, setCategory] = useState(null)
+    const API_URL = "https://json-server-backend-app.adaptable.app"
+    const { categoryID } = useParams()
+
+    useEffect(() => {
+        axios.get(`${API_URL}/categories`)
+            .then((response) => {
+                setCategory(response.data)
+            })
+            .catch(e => console.log(e))
+    }, [categoryID])
+
+    return (
+        <>
+            <h1>This is Dashboard</h1>
+
+            {category === null ? <p>Loading...</p> :
+                (category.map((element) => {
+                    return <Link to={`/categories/${element.id}`} key={element.id}><CategoryCard name={element.name} /></Link>
+
+
+                })
+                )}
+
+
+        </>
+    )
 }
